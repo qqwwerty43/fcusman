153e255a5bc92b0a28149251090c2d8eb42a579c
Bobr201 <99280076+qqwwerty43@users.noreply.github.com>
2024-03-05 14:33:26 +0300

    Initial commit

---
+++ blog/myblog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import MainView, PostDetailView, SignUpView, SignInView, SearchResultsView, TagView
+from .views import sign_out
+
+urlpatterns = [
+    path('', MainView.as_view(), name='index'),
+    path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('signout/', sign_out, name='signout'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('tag/<slug:slug>/', TagView.as_view(), name="tag"),
+]
